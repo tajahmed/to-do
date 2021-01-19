7a83d54f9248d18fa98a48eb10f25f2304d8f05c
tajahmed <m.t.a.78692@gmail.com>
2021-01-19 23:09:29 +0530

    new edit

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
           <input
             type='text'
             className='grocery'
-            placeholder='e.g. eggs'
+            placeholder='e.g. Books'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
